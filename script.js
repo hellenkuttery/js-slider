e7c22fac5d0b702499aa9b2a8b8a221162841d56
hellenkuttery <hellen.kut90@gmail.com>
2021-05-11 18:37:56 +0300

    meals are added

--- script.js
+++ script.js
@@ -13,9 +13,42 @@ const meal=[
         name:"Çikolata Kaplı Çilek",
         image:"./img/cikolata-kapli-cilek.jpg",
         link:"https://yemek.com/cikolatali-pratik-tatli-tarifleri/"
+    },
+    {
+        name:"Adana Kebap",
+        image:"./img/adana-kebap.jpg",
+        link:"https://yemek.com/tarif/adana-kebap/"
+    },
+    {
+        name:"Patatesli Börek",
+        image:"./img/patatesli-borek-tarif-son.jpg",
+        link:"https://yemek.com/tarif/patatesli-borek/"
+    },
+    {
+        name:"Şekerpare",
+        image:"./img/sekerpare-ytk-site.jpg",
+        link:"https://yemek.com/tarif/sekerpare/"
+    },
+    {
+        name:"Ev Usulü Pizza",
+        image:"./img/ev-usulu-pizza-yeni.jpg",
+        link:"https://yemek.com/tarif/ev-usulu-pizza/"
+    },
+    {
+        name:"Frambuazlı İrmik Tatlısı",
+        image:"./img/frambuazli-irmik-tatlisi-sunum-1.jpg",
+        link:"https://yemek.com/meyveli-sutlu-tatli-tarifleri/"
+    },
+    {
+        name:"Yoğurtlu Patates",
+        image:"./img/yogurtlu-patates-canaklari-tarifi.jpg",
+        link:"https://yemek.com/patatesli-mezeler/"
     }
 ];
-var counter=0;
+
+
+var counter=1;
+
 
 function slider(e){
     document.querySelector(".card-img-top").setAttribute("src",meal[e].image)
@@ -33,4 +66,10 @@ document.querySelector(".next").addEventListener("click",function(){
     console.log(counter);
     slider(counter)
 })
+
+setInterval(init,2000)
+function init(){
+    var randomImg=Math.floor(Math.random() * meal.length);
+    slider(randomImg)
+}
 
